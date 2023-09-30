dec6095260630228a36f0eedf9d8e3f3dc3eab2d
okodeee <120557382+okodeee@users.noreply.github.com>
2023-10-01 00:25:01 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+int gcd(int a, int b);
+int lcm(int a, int b);
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << gcd(a, b) << '\n' << lcm(a, b);
+}
+
+int gcd(int a, int b) {
+	int c = a % b;
+	while (c > 0) {
+		a = b;
+		b = c;
+		c = a % b;
+	}
+	return b;
+}
+
+int lcm(int a, int b) {
+	return (a * b) / gcd(a, b);
+}
