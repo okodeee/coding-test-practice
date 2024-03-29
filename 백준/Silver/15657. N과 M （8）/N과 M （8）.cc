650cb40abbd80a17846c03b4132799611c487dbd
okodeee <120557382+okodeee@users.noreply.github.com>
2024-03-29 17:54:42 +0900

    [Silver III] Title: N과 M (8), Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 9
+using namespace std;
+
+int N, M;
+int sequence[MAX];
+//bool visited[10001];
+int inputNum[MAX];
+
+void dfs(int cnt,int latestIdx);
+
+int main() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		cin >> inputNum[i];
+	}
+	sort(inputNum, inputNum + N);
+	dfs(0, 0);
+}
+
+void dfs(int cnt, int latestIdx) {
+	if (cnt == M) {
+		for (int index = 0; index < M; index++)
+			cout << sequence[index] << ' ';
+		cout << '\n';
+	}
+	else {
+		for (int index = latestIdx; index < N; index++) {
+			sequence[cnt] = inputNum[index];
+			dfs(cnt + 1, index);
+		}
+	}
+}
