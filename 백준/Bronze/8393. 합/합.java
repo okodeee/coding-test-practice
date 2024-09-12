f1f5427bd74b65de1947222c9a8d0472ef5d5e35
okodeee <120557382+okodeee@users.noreply.github.com>
2024-09-12 09:39:04 +0900

    [Bronze V] Title: 합, Time: 60 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -7,8 +7,8 @@ public class Main{
         int n = Integer.parseInt(br.readLine());
 
         int sum = 0;
-        for(int i = 0; i < n; i++){ // 해당 전월까지의 일수
-            sum += (i + 1);
+        for(int i = 1; i <= n; i++){ // 해당 전월까지의 일수
+            sum += i;
         }
 
         System.out.println(sum);
