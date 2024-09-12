e28e59e2bac19fdbc24d48a39a3d8b4520ee4582
okodeee <120557382+okodeee@users.noreply.github.com>
2024-09-12 09:38:34 +0900

    [Bronze V] Title: 합, Time: 64 ms, Memory: 11440 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        for(int i = 0; i < n; i++){ // 해당 전월까지의 일수
+            sum += (i + 1);
+        }
+
+        System.out.println(sum);
+    }
+}
