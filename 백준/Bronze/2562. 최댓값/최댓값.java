a3195928adfabaf16c9f5dccad6470170fa782a5
okodeee <120557382+okodeee@users.noreply.github.com>
2024-09-16 17:08:42 +0900

    [Bronze III] Title: 최댓값, Time: 64 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int max = 0;
+        int index = 0;
+        for(int i = 0; i < 9; i++){
+            if (arr[i] > max) {
+                max = arr[i];
+                index = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index + 1);
+    }
+}
