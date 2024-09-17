ac514d6fb531a3dd1887f91c4159ffebed674415
okodeee <120557382+okodeee@users.noreply.github.com>
2024-09-17 17:57:39 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 200 ms, Memory: 16204 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] arr_A = new int[101][101];
+        int[][] arr_B = new int[101][101];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++) {
+                arr_A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++) {
+                arr_B[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print((arr_A[i][j] + arr_B[i][j]) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
