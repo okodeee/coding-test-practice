1c979907adac9951fbd1618ac35a8ad710522de9
okodeee <120557382+okodeee@users.noreply.github.com>
2024-09-17 13:53:17 +0900

    [Silver V] Title: 색종이, Time: 68 ms, Memory: 11572 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[][] arr = new boolean[100][100];
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int x, y;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    arr[j][k] = true;
+                }
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (arr[i][j]) {
+                    sum++;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
