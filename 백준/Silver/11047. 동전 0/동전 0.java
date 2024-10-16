9d52f1ac986cb0ebda9b7c21478397b831d36f24
okodeee <120557382+okodeee@users.noreply.github.com>
2024-10-16 21:39:44 +0900

    [Silver IV] Title: 동전 0, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,28 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int n, k;
-    static int[] arr = new int[10];
-
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        for(int i=0; i<n; i++) arr[i] = Integer.parseInt(bf.readLine());
-
-        int ans = 0;
-        for(int i=n-1; i>=0; i--){
-            ans += k/arr[i];
-            k %= arr[i];
+        int[] coin = new int[N];
+        for (int i = 0; i < N; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
         }
 
-        System.out.println(ans);
+        int count = 0;
+        int sum = 0;
+        for (int i = N-1; i >= 0; i--) {
+            count = K / coin[i];
+            sum += count;
+            K -= coin[i] * count;
+        }
+        System.out.println(sum);
     }
 }
