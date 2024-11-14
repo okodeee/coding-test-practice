2068e278ef3eaa49e5ba7f6ff884b5bfd68d1410
okodeee <120557382+okodeee@users.noreply.github.com>
2024-11-14 23:48:44 +0900

    Update 블로그.java

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -2,48 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int X = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
         int[] visitors = new int[N];
-        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
             visitors[i] = Integer.parseInt(st.nextToken());
         }
 
-        int sum = 0;
+        int maxVisitors = 0;
+        int periodNum = 1;
+
         for (int i = 0; i < X; i++) {
-            sum += visitors[i];
+            maxVisitors += visitors[i];
         }
 
+        int tempVisitors = maxVisitors;
+        for (int i = 0; i < N - X; i++) {
+            tempVisitors -= visitors[i];
+            tempVisitors += visitors[i + X];
 
-        int max = sum;
-        int answer = 1;
-        for (int i = 0; i < N - X; i++) { // 슬라이딩 윈도우
-
-            //한칸 씩 이동
-            sum += visitors[i+X];
-            sum -= visitors[i];
-
-            if (sum == max) { // 최다 방문자수가 유지될 경우 answer++
-                answer++;
+            if (tempVisitors > maxVisitors) {
+                maxVisitors = tempVisitors;
+                periodNum = 1;
+            } else if (tempVisitors == maxVisitors) {
+                periodNum++;
             }
-
-            if (sum > max) { // 최다 방문자수가 갱신되었을 경우
-                answer = 1;
-                max = sum;
-            }
-
         }
-        if (max == 0) { // 최대 방문자 수가 0이라면 SAD 출력
+
+        if (maxVisitors == 0) {
             System.out.println("SAD");
-        } else {
-            System.out.println(max);
-            System.out.println(answer);
+            return;
         }
+        System.out.println(maxVisitors);
+        System.out.println(periodNum);
     }
 }
