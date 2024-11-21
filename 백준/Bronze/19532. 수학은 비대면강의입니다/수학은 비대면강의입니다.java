93796957476ff7398446d85455cb41ac2d5a78f0
okodeee <120557382+okodeee@users.noreply.github.com>
2024-11-21 16:08:55 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 92 ms, Memory: 12168 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -2,35 +2,25 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        br.close();
-
-        //첫번째 식
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
-        // 두번째 식
         int d = Integer.parseInt(st.nextToken());
         int e = Integer.parseInt(st.nextToken());
         int f = Integer.parseInt(st.nextToken());
 
-        //각각의 x, y가 -999 <= x, y <= 999이기 때문에
-        for(int i = -999; i <= 999; i++) { 
-            for(int j = -999; j <= 999; j++) {
-                if(a*i + b*j  == c){ // ax + by 가 C이고(x = i, y = j)
-                    if(d*i + e*j  == f) { // dx + ey가 f일 때(x = i, y = j)
-                        bw.write(i + " " + j + "\n");
-                        break;
-                    }
+        for (int x = -999; x <= 999; x++) {
+            for (int y = -999; y <= 999; y++) {
+                if (a * x + b * y == c && d * x + e * y == f) {
+                    System.out.println(x + " " + y);
+                    return;
                 }
             }
         }
-        bw.flush();
-        bw.close();
-
     }
 }
