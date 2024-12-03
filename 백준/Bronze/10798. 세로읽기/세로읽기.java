4b1c17e41de4769ec96d30fc415ced01c4378128
okodeee <120557382+okodeee@users.noreply.github.com>
2024-12-03 23:08:43 +0900

    [Bronze I] Title: 세로읽기, Time: 64 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,32 +1,23 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		char[][] c_arr = new char[5][15];
-		int i,j;
-		int max = 0;
-		
-		for(i=0;i<5;i++) {
-			String str = sc.next();
-			
-			if(str.length()>max) 
-				max = str.length();
-			
-			for(j=0;j<str.length();j++) {
-				c_arr[i][j] = str.charAt(j);
-			}
-		}
-		
-		
-		for(i=0;i<max;i++) {
-			for(j=0;j<5;j++) {
-				if(c_arr[j][i] == '\0') // 공백이 아니라 NULL이기 때문에 '\0'으로 
-					continue;
-				System.out.print(c_arr[j][i]);
-			}
-		}
-		sc.close();
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arr = new String[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = br.readLine();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[j].length() <= i)
+                    continue;
+                sb.append(arr[j].charAt(i));
+            }
+        }
+
+        System.out.println(sb);
+    }
 }
