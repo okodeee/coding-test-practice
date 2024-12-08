dff647d73c3f479b763673d32175c1723fd43e4d
okodeee <120557382+okodeee@users.noreply.github.com>
2024-12-08 14:59:01 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1676 ms, Memory: 95696 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,54 +1,52 @@
-import java.io.*;  
-import java.util.*;  
-  
-public class Main {  
-  
-    public static void main(String[] args) throws IOException {  
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
-        int n = Integer.parseInt(br.readLine());  
-        // 각 노드의 부모 노드를 저장하는 배열  
-        int[] parent = new int[n + 1];  
-        // 연결 리스트 배열  
-        ArrayList<Integer>[] adj = new ArrayList[n + 1];  
-        for (int i = 1; i <= n; i++) {  
-            adj[i] = new ArrayList<>();  
-        }  
-        // 방문 체크 배열  
-        boolean[] visited = new boolean[n + 1];  
-        StringTokenizer st;  
-        for (int i = 1; i < n; i++) {  
-            st = new StringTokenizer(br.readLine());  
-            int a = Integer.parseInt(st.nextToken());  
-            int b = Integer.parseInt(st.nextToken());  
-            adj[a].add(b);  
-            adj[b].add(a);  
-        }  
-  
-        // BFS를 위한 큐  
-        Queue<Integer> queue = new LinkedList<>();  
-        // 루트 노드인 1을 큐에 넣고 방문 처리  
-        queue.add(1);  
-        visited[1] = true;  
-        // BFS   
-while (!queue.isEmpty()) {  
-            int cur = queue.poll();  
-            // 현재 노드에 연결된 다음 노드들을 탐색  
-            for (int next : adj[cur]) {  
-                // 방문했던 노드라면 건너뛴다  
-                if (visited[next]) {  
-                    continue;  
-                }  
-                // 다음 노드를 방문 처리하고 큐에 넣는다.  
-                visited[next] = true;  
-                queue.add(next);  
-                // 다음 노드의 부모가 현재 노드임을 저장한다.  
-                parent[next] = cur;  
-            }  
-        }  
-  
-        // 부모 노드를 출력한다.  
-        for (int i = 2; i <= n; i++) {  
-            System.out.println(parent[i]);  
-        }  
-    }  
+import java.io.*;
+import java.util.*;
+
+/*
+ * 그래프를 인접리스트 형태로 구현한다.
+ * 인접 행렬 방식은 연결되지 않은 노드 정보도 포함해 메모리가 불필요하게 낭비된다.
+ * 인접 행렬로 구현 시 N이 최대일 때 100만 * 4B = 4GB
+ */
+public class Main {
+    static int N;
+    static ArrayList<Integer>[] adj;
+    static boolean[] visit;
+    static int[] parents;
+
+    public static void dfs(int V) {
+        visit[V] = true;
+
+        for (int i : adj[V]) {
+            if (!visit[i]) {
+                parents[i] = V;
+                dfs(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        visit = new boolean[N + 1];
+        parents = new int[N + 1];
+
+        adj = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        StringTokenizer st;
+        for (int i = 1; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            adj[v1].add(v2);
+            adj[v2].add(v1);
+        }
+        dfs(1);
+
+        for (int i = 2; i <= N; i++) {
+            System.out.println(parents[i]);
+        }
+    }
 }
