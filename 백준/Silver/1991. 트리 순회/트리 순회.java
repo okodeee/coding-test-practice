7029778e207dab6c8122fbaf5c58cd67627294f6
okodeee <120557382+okodeee@users.noreply.github.com>
2024-12-19 12:58:04 +0900

    [Silver I] Title: 트리 순회, Time: 64 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 class Node {
     char value;
@@ -44,21 +42,22 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        tree = new Node[N + 1];
 
+        int N = Integer.parseInt(br.readLine());
+        tree = new Node[N];
+
+        StringTokenizer st;
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine()); 
+            st = new StringTokenizer(br.readLine());
             char parentValue = st.nextToken().charAt(0);
             char leftValue = st.nextToken().charAt(0);
             char rightValue = st.nextToken().charAt(0);
-            
-            if (tree[parentValue - 'A'] == null) { 
+
+            if (tree[parentValue - 'A'] == null)
                 tree[parentValue - 'A'] = new Node(parentValue);
-            }
             if (leftValue != '.') {
                 tree[leftValue - 'A'] = new Node(leftValue);
-                tree[parentValue - 'A'].left = tree[leftValue - 'A']; 
+                tree[parentValue - 'A'].left = tree[leftValue - 'A'];
             }
             if (rightValue != '.') {
                 tree[rightValue - 'A'] = new Node(rightValue);
