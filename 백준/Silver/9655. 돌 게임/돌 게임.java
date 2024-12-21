5f369e5d1d96c9bd5a56636168554e2b8c05ce4e
okodeee <120557382+okodeee@users.noreply.github.com>
2024-12-21 14:17:58 +0900

    [Silver V] Title: 돌 게임, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,21 +1,12 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
 
-		int N = Integer.parseInt(br.readLine());
+        System.out.println(N % 2 == 0 ? "CY" : "SK");
 
-		bw.write((N % 2 == 1) ? "SK" : "CY");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-
+    }
 }
