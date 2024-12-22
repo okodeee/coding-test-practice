d8d8a67a058d444fc8ecc677726ee21dba71a99f
okodeee <120557382+okodeee@users.noreply.github.com>
2024-12-22 13:40:09 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 64 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,16 +1,29 @@
-import java.util.Scanner;
+import java.io.*;
 
+/*
+ * (n-1) 타일에 2*1 타일이 세로로 하나 붙음
+ * + (n-2) 타일에 1*2 타일이 가로로 두 개 붙음 + 2*2 타일이 붙음
+ * = dp[n] = dp[n-1] + 2*dp[n-2]
+ */
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        int[] dp = new int[1001];
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        // dp[2] 초기화를 위해 int[n] 대신 최대 값 넣음
+        dp = new int[1001];
         dp[1] = 1;
         dp[2] = 3;
-        for (int i = 3; i <= n; i++)
-            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
 
-        System.out.println(dp[n]);
+        System.out.println(tile(n));
+    }
+
+    public static int tile(int n) {
+        if (dp[n] == 0)
+            dp[n] = (tile(n - 1) + 2 * tile(n - 2)) % 10007;
+        return dp[n];
     }
 }
