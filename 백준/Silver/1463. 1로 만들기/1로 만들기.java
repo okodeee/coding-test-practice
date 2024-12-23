2251a4605e3a5ee90ff0ce9454321a5a60d3d87e
okodeee <120557382+okodeee@users.noreply.github.com>
2024-12-23 13:50:27 +0900

    [Silver III] Title: 1로 만들기, Time: 156 ms, Memory: 57176 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,45 +1,30 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
+import java.io.*;
+
 public class Main {
- 
-	static Integer[] dp;
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int N = Integer.parseInt(br.readLine());
- 
-		dp = new Integer[N + 1];
-		dp[0] = dp[1] = 0;
- 
-		System.out.print(recur(N));
- 
-	}
- 
-	static int recur(int N) {
- 
-		if (dp[N] == null) {
-			// 6으로 나눠지는 경우 
-			if (N % 6 == 0) {
-				dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
-			}
-			// 3으로만 나눠지는 경우 
-			else if (N % 3 == 0) {
-				dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
-			}
-			// 2로만 나눠지는 경우 
-			else if (N % 2 == 0) {
-				dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
-			}
-			// 2와 3으로 나누어지지 않는 경우
-			else {
-				dp[N] = recur(N - 1) + 1;
-			}
-		}
-		return dp[N];
-	}
- 
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        dp = new int[N + 1];
+        dp[0] = dp[1] = 0;
+
+        System.out.println(min(N));
+    }
+
+    public static int min(int N) {
+        if (N > 1 && dp[N] == 0) {
+            if (N % 6 == 0) {
+                dp[N] = Math.min(min(N / 3), Math.min(min(N / 2), min(N - 1))) + 1;
+            } else if (N % 3 == 0) {
+                dp[N] = Math.min(min(N / 3), min(N - 1)) + 1;
+            } else if (N % 2 == 0) {
+                dp[N] = Math.min(min(N / 2), min(N - 1)) + 1;
+            } else {
+                dp[N] = min(N - 1) + 1;
+            }
+        }
+        return dp[N];
+    }
 }
