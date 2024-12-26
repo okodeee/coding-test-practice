d121f2ecc62b307b25244d87ba003b0d10695211
okodeee <120557382+okodeee@users.noreply.github.com>
2024-12-26 14:06:36 +0900

    [Silver III] Title: 계단 오르기, Time: 68 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,42 +1,28 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
+import java.io.*;
+
 public class Main {
- 
-	static Integer dp[];
-	static int arr[];
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		dp = new Integer[N + 1];
-		arr = new int[N + 1];
-		
-		for(int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		
-		dp[0] = arr[0];	// 디폴트값이 null이므로 0으로 초기화 해주어야한다.
-		dp[1] = arr[1];
-		
-		if(N >= 2) {
-			dp[2] = arr[1] + arr[2];
-		}
-		
-		System.out.println(find(N));
-		
-	}
-	
-	static int find(int N) {
-		// 아직 탐색하지 않는 N번째 계단일 경우
-		if(dp[N] == null) {
-			dp[N] = Math.max(find(N - 2), find(N - 3) + arr[N - 1]) + arr[N];
-		}
-		
-		return dp[N];
-	}
-	 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] score = new int[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            score[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[][] step = new int[3][n+1];
+
+        step[1][1] = score[1];
+        if (n > 1) {
+            step[1][2] = score[2];
+            step[2][2] = step[1][1] + score[2];
+        }
+        for (int i = 3; i <= n; i++) {
+            step[1][i] = Math.max(step[1][i-2], step[2][i-2]) + score[i];
+            step[2][i] = step[1][i-1] + score[i];
+        }
+        System.out.println(Math.max(step[1][n], step[2][n]));
+
+    }
 }
