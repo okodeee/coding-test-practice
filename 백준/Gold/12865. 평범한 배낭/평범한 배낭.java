dd4d1fc75d9654dc0f851433b30e11b31cb5095f
okodeee <120557382+okodeee@users.noreply.github.com>
2024-12-30 16:20:28 +0900

    [Gold V] Title: 평범한 배낭, Time: 144 ms, Memory: 51392 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,56 +1,34 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.StringTokenizer;
- 
+
 public class Main {
- 
-	static Integer[][] dp;
-	static int[] W; // weight
-	static int[] V; // value
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
- 
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
- 
-		W = new int[N];
-		V = new int[N];
- 
-		dp = new Integer[N][K + 1];
- 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			W[i] = Integer.parseInt(st.nextToken());
-			V[i] = Integer.parseInt(st.nextToken());
-		}
- 
-		System.out.println(knapsack(N - 1, K));
- 
-	}
- 
-	static int knapsack(int i, int k) {
-		// i가 0미만, 즉 범위 밖이 된다면
-		if (i < 0)
-			return 0;
-		
-		// 탐색하지 않은 위치라면?
-		if (dp[i][k] == null) {
-			
-			// 현재 물건(i)을 추가로 못담는 경우 (이전 i값 탐색) 
-			if(W[i] > k) {
-				dp[i][k] = knapsack(i - 1, k);
-			}
-			// 현재 물건(i)을 담을 수 있는 경우 
-			else {
-				// 이전 i값과 이전 i값에 대한 k-W[i]의 값 + 현재 가치(V[i])중 큰 값을 저장  
-				dp[i][k] = Math.max(knapsack(i - 1, k), knapsack(i - 1, k - W[i]) + V[i]);
-			}
-		}
-		return dp[i][k];
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken()); // 물건의 개수
+        int K = Integer.parseInt(st.nextToken()); // 배낭의 최대 무게
+        int[] W = new int[N + 1];
+        int[] V = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            W[i] = Integer.parseInt(st.nextToken()); // 무게
+            V[i] = Integer.parseInt(st.nextToken()); // 가치
+        }
+
+        int[][] dp = new int[N + 1][K + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int w = 0; w <= K; w++) {
+                if (W[i] > w) {
+                    dp[i][w] = dp[i - 1][w]; // 물건을 넣을 수 없을 때
+                } else {
+                    dp[i][w] = Math.max(dp[i - 1][w], V[i] + dp[i - 1][w - W[i]]);
+                }
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
 }
