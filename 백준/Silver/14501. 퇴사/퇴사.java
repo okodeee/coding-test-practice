41279d0921943a41d3329749dfac8dc5a60baa07
okodeee <120557382+okodeee@users.noreply.github.com>
2024-12-30 17:23:29 +0900

    [Silver III] Title: 퇴사, Time: 96 ms, Memory: 12884 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,29 +1,27 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-	
-	static int max=0;
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		int Case = sc.nextInt();
-		int[] T = new int[Case];//소요 기간
-		int[] P = new int[Case];//금액
-		for(int i=0;i<Case;i++) {
-			T[i]=sc.nextInt();	
-			P[i]=sc.nextInt();	
-		}//for
-		
-		int[] dp = new int[Case+1];
-		
-		for(int i=0;i<Case;i++) {
-			 if(i+T[i]<=Case) {	//범위에 벗어나지 않는다면 
-				 dp[i+T[i]]=Math.max(dp[i+T[i]],dp[i]+P[i]);	
-			 }//if
-			 dp[i+1]=Math.max(dp[i+1],dp[i]);	//다음dp=현재 누적값vs 다음 누적값
-			 
-		}//for	
-		System.out.println(dp[Case]);	
-	}
-	
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] T = new int[N + 1];
+        int[] P = new int[N + 1];
+        int[] dp = new int[N + 2];
+
+        for (int i = 1; i <= N; i++) {
+            T[i] = sc.nextInt();
+            P[i] = sc.nextInt();
+        }
+
+        for (int i = N; i > 0; i--) {
+            if (i + T[i] <= N + 1) {
+                dp[i] = Math.max(dp[i + 1], P[i] + dp[i + T[i]]);
+            } else {
+                dp[i] = dp[i + 1];
+            }
+        }
+
+        System.out.println(dp[1]);
+        sc.close();
+    }
 }
