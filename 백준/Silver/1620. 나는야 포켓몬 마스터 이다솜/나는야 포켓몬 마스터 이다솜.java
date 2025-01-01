35ef065f1649b89c44a4b5db1e3164a53690eb77
okodeee <120557382+okodeee@users.noreply.github.com>
2025-01-01 16:27:29 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 1340 ms, Memory: 58204 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,38 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		StringBuilder sb = new StringBuilder();
-		//조건에 따라 다르게 쓰일 두개의 HashMap
-		HashMap<Integer, String> hash1 = new HashMap<Integer, String>();
-		HashMap<String, Integer> hash2 = new HashMap<String, Integer>();
-		
-		
-		for(int i = 1; i <= N; i++) {
-			String S = br.readLine();
-			hash1.put(i, S);
-			hash2.put(S, i);
-		}
-		
-		for(int i = 0; i < M; i++) {
-			String S = br.readLine();
-			//입력값이 번호인지 포켓몬이름인지 판별
-			if(49 <= S.charAt(0) && S.charAt(0) <= 57) {
-				sb.append(hash1.get(Integer.parseInt(S))).append("\n");
-			}else {
-				sb.append(hash2.get(S)).append("\n");
-			}
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // (key : value)가 (이름, 번호), (번호, 이름)인 hashmap 두 개 생성
+        HashMap<Integer, String> map1 = new HashMap<>();
+        HashMap<String, Integer> map2 = new HashMap<>();
+
+        String name;
+        for (int i = 1; i <= N; i++) {
+            name = br.readLine();
+            map1.put(i, name);
+            map2.put(name, i);
+        }
+
+        String q;
+        for (int i = 0; i < M; i++) {
+            q = br.readLine();
+            if (q.charAt(0) >= 'A') {
+                System.out.println(map2.get(q));
+            } else {
+                System.out.println(map1.get(Integer.parseInt(q)));
+            }
+        }
+    }
 }
