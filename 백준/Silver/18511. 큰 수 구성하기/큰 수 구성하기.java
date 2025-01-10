ce606dde36cedbe94fcf36135dbf89d1b9ee0118
okodeee <120557382+okodeee@users.noreply.github.com>
2025-01-10 15:27:20 +0900

    [Silver V] Title: 큰 수 구성하기, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.java
+++ 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.java
@@ -2,28 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, k, ans;
+    static int N, K, result;
     static int[] arr;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        arr = new int[k];
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arr = new int[K];
         st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < K; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
         dfs(0);
-        System.out.println(ans);
+        System.out.println(result);
     }
 
     public static void dfs(int total) {
-        if (total > n) return; // n보다 크면 종료
-        if (ans < total) ans = total; // 더 큰 값으로 ans 갱신
+        if (total > N) return; // n보다 크면 종료
+        if (result < total) result = total; // 더 큰 값으로 ans 갱신
 
-        for (int i = k - 1; i >= 0; i--) {
+        for (int i = K - 1; i >= 0; i--) {
             dfs(total * 10 + arr[i]);
         }
     }
