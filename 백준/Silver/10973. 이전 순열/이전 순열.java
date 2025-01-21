876f23349558b645026e399dcd1a3fd7310ab0a8
okodeee <120557382+okodeee@users.noreply.github.com>
2025-01-21 10:31:01 +0900

    [Silver III] Title: 이전 순열, Time: 284 ms, Memory: 18372 KB -BaekjoonHub

--- 백준/Silver/10973. 이전 순열/이전 순열.java
+++ 백준/Silver/10973. 이전 순열/이전 순열.java
@@ -1,53 +1,79 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
+/*
+ * 1. 뒤에서부터 첫 증가 지점 찾기
+ * 2. i 위치의 값과 교체할 값 찾기
+ * 3. 값 교체
+ * 4. i 이후의 순열 정리
+ */
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		
-		int[] arr = new int[N];
-		for(int i = 0; i < N; i++)
-			arr[i] = sc.nextInt();
-	
-		
-		if(prePermutaion(arr)) {
-			for(int i : arr)
-				System.out.print(i + " ");
-		} else {
-			System.out.println(-1);
-		}
-	}
-	
-	public static boolean prePermutaion(int[] arr) {
-		int i = arr.length-1;
-		
-		while(i > 0 && arr[i] > arr[i-1]) { //부등호 수정
-			i--;
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		if(i == 0) //첫 번째 순열인 경우
-			return false;
-		
-		int j = arr.length-1;
-		while(arr[i-1] < arr[j]) { //부등호 수정
-			j--;
-		}
-		swap(arr, i-1, j);
-		
-		j = arr.length-1;
-		while(i < j) {
-			swap(arr, i, j);
-			i++;
-			j--;
-		}
-		
-		return true;
-	}
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
 
-	public static void swap(int[] arr, int i, int j) {
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-	}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
 
+        // 이전 순열 계산
+        if (prevPermutation(nums)) {
+            // 결과 출력
+            for (int num : nums) {
+                System.out.print(num + " ");
+            }
+        } else {
+            // 이전 순열이 없는 경우
+            System.out.println("-1");
+        }
+    }
+
+    // 다음 순열을 구하는 함수
+    public static boolean prevPermutation(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: 뒤에서부터 첫 번째 증가 지점 찾기
+        int i = n - 2;
+        while (i >= 0 && nums[i] <= nums[i + 1]) {
+            i--;
+        }
+
+        // 증가 지점을 못 찾은 경우 (첫번째 순열)
+        if (i < 0) {
+            return false;
+        }
+
+        // Step 2: i 이후에서 교체할 값 찾기
+        int j = n - 1;
+        while (nums[j] >= nums[i]) {
+            j--;
+        }
+
+        // Step 3: 값 교환
+        swap(nums, i, j);
+
+        // Step 4: i 이후의 배열 정렬 (역순으로 뒤집기)
+        reverse(nums, i + 1, n - 1);
+
+        return true;
+    }
+
+    // 두 값을 교환하는 함수
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    // 배열을 뒤집는 함수
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
 }
