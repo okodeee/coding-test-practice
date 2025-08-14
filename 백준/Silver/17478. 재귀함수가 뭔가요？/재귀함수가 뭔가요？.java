c2ca98b70389cf398a51d79d3968858a37309b1d
okodeee <120557382+okodeee@users.noreply.github.com>
2025-08-14 11:54:36 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 116 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -1,43 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int n;
-
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
+        int N =  Integer.parseInt(br.readLine());
 
-        System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+        sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+        rec(N, 0);
 
-        recur(0, "");
-
+        System.out.println(sb);
     }
 
-    public static void recur(int depth, String under) {
-//        String under = "";
-//        for (int i = 1; i <= depth; i++) {
-//            under += "____";
-//        }
+    static void rec(int num, int blank) {
+        if (num == 0) {
+            for (int i = 0; i < blank; i++) {
+                sb.append("____");
+            }
+            sb.append("\"재귀함수가 뭔가요?\"\n");
 
-        if (depth == n) {
-            System.out.println(under + "\"재귀함수가 뭔가요?\"");
-            System.out.println(under + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
-            System.out.println(under + "라고 답변하였지.");
-            return;
+            for (int i = 0; i < blank; i++) {
+                sb.append("____");
+            }
+            sb.append("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
+        } else {
+            for (int i = 0; i < blank; i++) {
+                sb.append("____");
+            }
+            sb.append("\"재귀함수가 뭔가요?\"\n");
 
+            for (int i = 0; i < blank; i++) {
+                sb.append("____");
+            }
+            sb.append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+
+            for (int i = 0; i < blank; i++) {
+                sb.append("____");
+            }
+            sb.append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+
+            for (int i = 0; i < blank; i++) {
+                sb.append("____");
+            }
+            sb.append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+
+            rec(num - 1, blank + 1);
         }
 
-        System.out.println(under + "\"재귀함수가 뭔가요?\"");
-        System.out.println(under + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
-        System.out.println(under + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-        System.out.println(under + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
-
-
-        recur(depth + 1, under + "____");
-        System.out.println(under + "라고 답변하였지.");
+        for (int i = 0; i < blank; i++) {
+            sb.append("____");
+        }
+        sb.append("라고 답변하였지.\n");
     }
-
 }
