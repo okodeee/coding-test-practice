64e6bac4e40e9fe4981c895afbcc876e58f5f2fa
okodeee <120557382+okodeee@users.noreply.github.com>
2025-09-05 10:09:27 +0900

    [Gold IV] Title: 스도쿠, Time: 884 ms, Memory: 22164 KB -BaekjoonHub

---
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -0,0 +1,91 @@
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] board = new int[9][9];
+
+        String str;
+        int zeroCount = 0;
+        int num;
+        for (int i = 0; i < 9; i++) {
+            str = br.readLine();
+            for (int j = 0; j < 9; j++) {
+                num = str.charAt(j) - '0';
+                board[i][j] = num;
+                if (num == 0) {
+                    zeroCount++;
+                }
+            }
+        }
+        
+        if (sudoku(board, zeroCount)) {
+            printBoard(board);
+        }
+
+    }
+
+    static boolean sudoku(int[][] board, int termination) {
+        if (termination == 0) {
+            return true;
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == 0) {
+                    for (int k = 1; k <= 9; k++) {
+                        board[i][j] = k;
+
+                        if (promising(board, i, j)) {
+                            if (sudoku(board, termination - 1)) {
+                                return true;
+                            }
+                        }
+                        board[i][j] = 0;
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    static boolean promising(int[][] board, int x, int y) {
+        int num = board[x][y]; // 검사할 숫자
+
+        // 가로줄 검사
+        for (int j = 0; j < 9; j++) {
+            if (j != y && board[x][j] == num) return false;
+        }
+
+        // 세로줄 검사
+        for (int i = 0; i < 9; i++) {
+            if (i != x && board[i][y] == num) return false;
+        }
+
+        // 정사각형 검사
+        int startX = (x / 3) * 3;
+        int startY = (y / 3) * 3;
+
+        for (int i = startX; i < startX + 3; i++) {
+            for (int j = startY; j < startY + 3; j++) {
+                if (!(i == x && j == y) && board[i][j] == num) return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void printBoard(int[][] board) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                sb.append(board[i][j]);
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
